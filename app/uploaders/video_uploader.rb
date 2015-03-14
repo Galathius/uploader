2c270760a6fe2e82369bda014e9bea351a585728
Ilya Kamenko <i.kamenko@itransition.com>
2015-03-14 14:15:46 +0300

    Added extension white list for VideoUploader

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -5,6 +5,10 @@ class VideoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_white_list
+    %w(avi mov mkv mpg mpeg mp4 m4v flv)
+  end
+
   def filename
     if original_filename
       @name ||= Digest::MD5.hexdigest(File.dirname(current_path))
